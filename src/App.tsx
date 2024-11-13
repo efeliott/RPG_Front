08d17733f5aea080ca6f611297630bf0092f8363
Eliott Fertille <eliottfertille@MacBook-Pro-de-Eliott.local>
2024-11-13 20:54:55 +0100

    correction de bug pour génération npm run build

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignIn from './components/Auth/SignIn';
-import SignUp from './components/Auth/Signup';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/Signup';
 import Dashboard from './pages/Dashboard';
 import SessionManage from './pages/SessionManage';
 import GameMasterManagement from './pages/GameMasterManagment';
 import { AuthProvider } from './context/AuthContext';
-import ProtectedLayout from './components/Layout/ProtectedLayout';
+import ProtectedLayout from './components/layout/ProtectedLayout';
 import JoinSession from './components/Sessions/JoinSession';
 import PlayerManagement from './pages/PlayerManagement'; 
 // import NotFound from './components/pages/NotFound'; // Composant NotFound pour les routes non trouvées
